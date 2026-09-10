89e9a627a3fbfcc3e4f038a76fc72cd49e75ef92
kitoumasaomi <kanfutrooper@gmail.com>
2026-09-10 20:59:47 +0900

    Introduction1を作成

--- Introduction1/htdocs1/blog.php
+++ Introduction1/htdocs1/blog.php
@@ -2,10 +2,13 @@
 
 $blog = $_POST;
 
-foreach($blog2 as $key => $value){
-    echo '<pre>';
-    echo $key.'='.htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
-    echo '</pre>';
+if($blog['publish_status'] === 'un_publish'){
+    foreach($blog2 as $key => $value){
+        echo '<pre>';
+        echo $key.'='.htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+        echo '</pre>';
+        }
+} else {
+    echo '公開中の記事がありません';
 }
-
 ?>
