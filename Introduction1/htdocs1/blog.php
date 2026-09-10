b453f57353219d720fea27bacaba061be479d012
kitoumasaomi <kanfutrooper@gmail.com>
2026-09-10 20:43:41 +0900

    Introduction1を作成

--- Introduction1/htdocs1/blog.php
+++ Introduction1/htdocs1/blog.php
@@ -4,7 +4,7 @@ $blog = $_POST;
 
 foreach($blog2 as $key => $value){
     echo '<pre>';
-    echo $key.'='.$value;
+    echo $key.'='.htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
     echo '</pre>';
 }
 
