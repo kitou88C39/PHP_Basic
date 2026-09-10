da0b6cd97ec952cf0511479bd63946c7fd43171a
kitoumasaomi <kanfutrooper@gmail.com>
2026-09-10 21:07:52 +0900

    Introduction1を作成

--- Introduction1/htdocs1/blog.php
+++ Introduction1/htdocs1/blog.php
@@ -7,8 +7,11 @@ if($blog['publish_status'] === 'un_publish'){
         echo '<pre>';
         echo $key.'='.htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
         echo '</pre>';
-        }
-} else {
+    }
+} elseif($blog['publish_status'] === 'un_publish') {
     echo '公開中の記事がありません';
+} else {
+    echo '記事がありません';
 }
+
 ?>
