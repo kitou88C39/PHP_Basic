360a5feca22b9e099088dddbca3e71667990ae73
kitoumasaomi <kanfutrooper@gmail.com>
2026-09-18 20:38:01 +0900

    reading-log-appを作成

--- reading-log-app/part2/src/book_log.php
+++ reading-log-app/part2/src/book_log.php
@@ -1,9 +1,9 @@
 <?php
 
-echo '1. 読書ログを登録してください'　. PHP_EOL;
-echo '1. 読書ログを登録してください'　. PHP_EOL;
-echo '1. 読書ログを登録してください'　. PHP_EOL;
-
+echo '1. 読書ログを登録'　. PHP_EOL;
+echo '2. 読書ログを表示'　. PHP_EOL;
+echo '9. アプリケーションを終了'　. PHP_EOL;
+echo '番号を選択してください(1,2,9)　　:'　. PHP_EOL;
 
 
 
