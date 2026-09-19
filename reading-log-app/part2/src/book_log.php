bd11218ac32f7ac2ae898e0425ae6c2f04c51605
kitoumasaomi <kanfutrooper@gmail.com>
2026-09-19 15:41:59 +0900

    reading-log-appを作成

--- reading-log-app/part2/src/book_log.php
+++ reading-log-app/part2/src/book_log.php
@@ -3,8 +3,7 @@
 echo '1. 読書ログを登録'　. PHP_EOL;
 echo '2. 読書ログを表示'　. PHP_EOL;
 echo '9. アプリケーションを終了'　. PHP_EOL;
-echo '番号を選択してください(1,2,9)　:'　.
-PHP_EOL;
+echo '番号を選択してください(1,2,9)　:';
 $num = trim(fgets(STDIN));
 
 if ($num === '1') {
@@ -26,6 +25,11 @@ $author = trim(fgets(STDIN));
 echo '読書状況(未読、読んでる、読了):';
 $status = trim(fgets(STDIN));
 
+echo '評価(5点満点の整数):';
+$score = trim(fgets(STDIN));
+
+echo '感想:';
+$summary = trim(fgets(STDIN));
 
 echo '読書ログを登録します'　. PHP_EOL;
 echo '書籍名:' . $title　. PHP_EOL;
