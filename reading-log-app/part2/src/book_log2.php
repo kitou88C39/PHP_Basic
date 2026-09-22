f7d01765fe4d1d8081b50b768076d1c2122591bc
kitoumasaomi <kanfutrooper@gmail.com>
2026-09-22 10:41:28 +0900

    reading-log-appを作成

---
+++ reading-log-app/part2/src/book_log2.php
@@ -0,0 +1,31 @@
+<?php
+
+function createReview()
+{
+
+        echo '読書ログを登録してください' . PHP_EOL;
+        echo '書籍名:';
+        $title = trim(fgets(STDIN));
+
+        echo '著者名:' . PHP_EOL . PHP_EOL;
+        $author = trim(fgets(STDIN));
+
+        echo '読書状況(未読、読んでる、読了):';
+        $status = trim(fgets(STDIN));
+
+        echo '評価(5点満点の整数):';
+        $score = trim(fgets(STDIN));
+
+        echo '感想:';
+        $summary = trim(fgets(STDIN));
+
+        $reviews[] = [
+        'title' => $title,
+        'author' => $author,
+        'status' => $status,
+        'score' => $score,
+        'summary' => $summary,
+        ];
+
+        echo '登録が完了しました' . PHP_EOL . PHP_EOL;
+  }
