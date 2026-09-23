ac5065d24310f4115930984634c59269b1bebec5
kitoumasaomi <kanfutrooper@gmail.com>
2026-09-23 19:31:42 +0900

    reading-log-appを作成

--- reading-log-app/part2/src/tmp/mysql.php
+++ reading-log-app/part2/src/tmp/mysql.php
@@ -4,7 +4,9 @@ $link = mysqli_connect('db', 'book_log',
 'pass', 'book_log');
 
 if(!$link){
-  echo 'Error: 'データベースに接続できませんでした'
+  echo 'Error: 'データベースに接続できませんでした' . PHP_EOL;
+  echo 'Debugging error: ' . mysqli_connect_error() . PHP_EOL;
+}
 }
 
 echo 'データベースに接続できました' . PHP_EOL;
