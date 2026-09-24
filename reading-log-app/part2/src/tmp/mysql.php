510875f7d086dd13be07c385f778ed9ab51f7e52
kitoumasaomi <kanfutrooper@gmail.com>
2026-09-24 20:55:16 +0900

    Introduction1を作成

--- reading-log-app/part2/src/tmp/mysql.php
+++ reading-log-app/part2/src/tmp/mysql.php
@@ -3,32 +3,34 @@
 $link = mysqli_connect('db', 'book_log', 'pass', 'book_log');
 
 if (!$link) {
-  echo 'Error: 'データベースに接続できませんでした' . PHP_EOL;
-  echo 'Debugging error: ' . mysqli_connect_error() . PHP_EOL;
-  exit;
+    echo 'Error: データベースに接続できませんでした' . PHP_EOL;
+    echo 'Debugging error: ' . mysqli_connect_error() . PHP_EOL;
+    exit;
 }
 
 echo 'データベースに接続できました' . PHP_EOL;
 
 $sql = <<<EOT
 INSERT INTO companies(
-  name,
-  establishment_date,
-  founder
+    name,
+    establishment_date,
+    founder
 ) VALUES (
-  'SmartHR Inc',
-  '2023-01-25',
-  'Shoji Miyata'
+    'SmartHR Inc',
+    '2023-01-25',
+    'Shoji Miyata'
 )
 EOT;
 
-$result = mysqli_query($linkm $sql);
-if ($result){
-  echo 'データベースに追加しました' . PHP_EOL;
+$result = mysqli_query($link, $sql);
+
+if ($result) {
+    echo 'データベースに追加しました' . PHP_EOL;
 } else {
-  echo 'Error: 'データベースに追加に失敗しました' . PHP_EOL;
-  echo 'Debugging error: ' . mysqli_error($link) . PHP_EOL;
+    echo 'Error: データベースへの追加に失敗しました' . PHP_EOL;
+    echo 'Debugging error: ' . mysqli_error($link) . PHP_EOL;
 }
 
 mysqli_close($link);
-echo 'データベースに接続を切断しました' . PHP_EOL;
+
+echo 'データベースとの接続を切断しました' . PHP_EOL;
