40aa323a942404ee75b18b6ffd1a8020f5cc7e74
kitoumasaomi <kanfutrooper@gmail.com>
2026-09-24 20:34:45 +0900

    Introduction1を作成

--- reading-log-app/part2/src/tmp/mysql.php
+++ reading-log-app/part2/src/tmp/mysql.php
@@ -1,15 +1,20 @@
 <?php
 
-$link = mysqli_connect('db', 'book_log',
-'pass', 'book_log');
+$link = mysqli_connect('db', 'book_log', 'pass', 'book_log');
 
-if (!$link) 
+if (!$link) {
   echo 'Error: 'データベースに接続できませんでした' . PHP_EOL;
   echo 'Debugging error: ' . mysqli_connect_error() . PHP_EOL;
   exit;
 }
 
-
 echo 'データベースに接続できました' . PHP_EOL;
+
+$sql = <<<EOT
+EOT;
+
+mysqli_query($linkm $sql);
+
+
 mysqli_close($link);
 echo 'データベースに接続を切断しました' . PHP_EOL;
