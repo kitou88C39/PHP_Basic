f46787bd989585882697e2ca9a46a113f9522d9b
kitoumasaomi <kanfutrooper@gmail.com>
2026-09-25 23:37:14 +0900

    Introduction1を作成

--- reading-log-app/part2/src/book_log3.php
+++ reading-log-app/part2/src/book_log3.php
@@ -2,32 +2,7 @@
 
 function createReview()
 {
-    echo '読書ログを登録してください' . PHP_EOL;
-
-    echo '書籍名:';
-    $title = trim(fgets(STDIN));
-
-    echo '著者名:';
-    $author = trim(fgets(STDIN));
-
-    echo '読書状況(未読、読んでる、読了):';
-    $status = trim(fgets(STDIN));
-
-    echo '評価(5点満点の整数):';
-    $score = trim(fgets(STDIN));
-
-    echo '感想:';
-    $summary = trim(fgets(STDIN));
-
-    echo '登録が完了しました' . PHP_EOL . PHP_EOL;
-
-    return [
-        'title' => $title,
-        'author' => $author,
-        'status' => $status,
-        'score' => $score,
-        'summary' => $summary,
-    ];
+    
 }
 
 $reviews = [];
@@ -36,33 +11,61 @@ while (true) {
     echo '1. 読書ログを登録' . PHP_EOL;
     echo '2. 読書ログを表示' . PHP_EOL;
     echo '9. アプリケーションを終了' . PHP_EOL;
-    echo '番号を選択してください(1,2,9):';
-
+    echo '番号を選択してください(1,2,9) :';
     $num = trim(fgets(STDIN));
 
     if ($num === '1') {
-
-        // 読書ログを登録
         $reviews[] = createReview();
 
+        echo '読書ログを登録してください' . PHP_EOL;
+        echo '書籍名:';
+        $title = trim(fgets(STDIN));
+
+        echo '著者名:' . PHP_EOL . PHP_EOL;
+        $author = trim(fgets(STDIN));
+
+        echo '読書状況(未読、読んでる、読了):';
+        $status = trim(fgets(STDIN));
+
+        echo '評価(5点満点の整数):';
+        $score = trim(fgets(STDIN));
+
+        echo '感想:';
+        $summary = trim(fgets(STDIN));
+
+        $reviews[] = [
+        'title' => $title,
+        'author' => $author,
+        'status' => $status,
+        'score' => $score,
+        'summary' => $summary,
+        ];
+
+        echo '登録が完了しました' . PHP_EOL . PHP_EOL;
     } elseif ($num === '2') {
-
-        // 読書ログを表示
         echo '登録されている読書ログを表示します' . PHP_EOL;
-
-        foreach ($reviews as $review) {
-            echo '書籍名:' . $review['title'] . PHP_EOL;
-            echo '著者名:' . $review['author'] . PHP_EOL;
-            echo '読書状況:' . $review['status'] . PHP_EOL;
-            echo '評価:' . $review['score'] . PHP_EOL;
-            echo '感想:' . $review['summary'] . PHP_EOL;
-            echo PHP_EOL;
-        }
-
     } elseif ($num === '9') {
-
-        // アプリケーションを終了
-        echo 'アプリケーションを終了します' . PHP_EOL;
-        break;
+        //アプリケーションを終了する
+        echo '書籍名:' .$reviews['title'] .
+        PHP_EOL;
     }
-}
+
+
+echo '読書ログを登録してください' . PHP_EOL;
+echo '書籍名:';
+$title = trim(fgets(STDIN));
+
+echo '著者名:' . PHP_EOL . PHP_EOL;
+$author = trim(fgets(STDIN));
+
+echo '読書状況(未読、読んでる、読了):';
+$status = trim(fgets(STDIN));
+
+echo '評価(5点満点の整数):';
+$score = trim(fgets(STDIN));
+
+echo '感想:';
+$summary = trim(fgets(STDIN));
+
+echo '読書ログを登録します' . PHP_EOL;
+echo '書籍名:' . $title . PHP_EOL;
