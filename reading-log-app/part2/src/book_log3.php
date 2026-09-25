ac656b4457e3b761c9b2156c1d6febecc6d823b8
kitoumasaomi <kanfutrooper@gmail.com>
2026-09-25 23:42:20 +0900

    Introduction1を作成

--- reading-log-app/part2/src/book_log3.php
+++ reading-log-app/part2/src/book_log3.php
@@ -2,70 +2,24 @@
 
 function createReview()
 {
-    
+
+  echo '読書ログを登録してください' . PHP_EOL;
+  echo '書籍名:';
+  $title = trim(fgets(STDIN));
+
+  echo '著者名:' . PHP_EOL . PHP_EOL;
+  $author = trim(fgets(STDIN));
+
+  echo '読書状況(未読、読んでる、読了):';
+  $status = trim(fgets(STDIN));
+
+  echo '評価(5点満点の整数):';
+  $score = trim(fgets(STDIN));
+
+  echo '感想:';
+  $summary = trim(fgets(STDIN));
+
+  echo '読書ログを登録します' . PHP_EOL;
+  echo '書籍名:' . $title . PHP_EOL;
+
 }
-
-$reviews = [];
-
-while (true) {
-    echo '1. 読書ログを登録' . PHP_EOL;
-    echo '2. 読書ログを表示' . PHP_EOL;
-    echo '9. アプリケーションを終了' . PHP_EOL;
-    echo '番号を選択してください(1,2,9) :';
-    $num = trim(fgets(STDIN));
-
-    if ($num === '1') {
-        $reviews[] = createReview();
-
-        echo '読書ログを登録してください' . PHP_EOL;
-        echo '書籍名:';
-        $title = trim(fgets(STDIN));
-
-        echo '著者名:' . PHP_EOL . PHP_EOL;
-        $author = trim(fgets(STDIN));
-
-        echo '読書状況(未読、読んでる、読了):';
-        $status = trim(fgets(STDIN));
-
-        echo '評価(5点満点の整数):';
-        $score = trim(fgets(STDIN));
-
-        echo '感想:';
-        $summary = trim(fgets(STDIN));
-
-        $reviews[] = [
-        'title' => $title,
-        'author' => $author,
-        'status' => $status,
-        'score' => $score,
-        'summary' => $summary,
-        ];
-
-        echo '登録が完了しました' . PHP_EOL . PHP_EOL;
-    } elseif ($num === '2') {
-        echo '登録されている読書ログを表示します' . PHP_EOL;
-    } elseif ($num === '9') {
-        //アプリケーションを終了する
-        echo '書籍名:' .$reviews['title'] .
-        PHP_EOL;
-    }
-
-
-echo '読書ログを登録してください' . PHP_EOL;
-echo '書籍名:';
-$title = trim(fgets(STDIN));
-
-echo '著者名:' . PHP_EOL . PHP_EOL;
-$author = trim(fgets(STDIN));
-
-echo '読書状況(未読、読んでる、読了):';
-$status = trim(fgets(STDIN));
-
-echo '評価(5点満点の整数):';
-$score = trim(fgets(STDIN));
-
-echo '感想:';
-$summary = trim(fgets(STDIN));
-
-echo '読書ログを登録します' . PHP_EOL;
-echo '書籍名:' . $title . PHP_EOL;
